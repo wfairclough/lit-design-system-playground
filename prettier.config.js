f862574ac74ee5e11c44b9b7ea66589d47c9f110
Will Fairclough <wfairclough@gmail.com>
2023-10-07 13:19:37 -0400

    Initial Commit

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+export default {
+  printWidth: 120,
+  // tabWidth: 2,
+  // useTabs: false,
+  // semi: true,
+  singleQuote: true,
+  // jsxSingleQuote: false,
+  trailingComma: 'all',
+  // bracketSpacing: true,
+  // jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  // rangeStart: 0,
+  // rangeEnd: Infinity,
+  // requirePragma: false,
+  // insertPragma: false,
+  // proseWrap: 'preserve',
+  // htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+};
